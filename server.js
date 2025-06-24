136c9921ccc7f6d4f3e71402c76140fcd5ef29cb
mohib41 <mkskd41@gmail.com>
2025-06-24 14:18:46 +0530

    update cannot get

--- server.js
+++ server.js
@@ -117,3 +117,8 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
+const fs = require('fs');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
